3f80899e3a7b39403e8cec1d090f70cbc6434452
vanla22 <anhhvan2212@gmail.com>
2021-11-21 20:17:35 +0700

    vanla17398_Auth-Msg-Xdate

---
+++ src/qlchs/utils/Auth.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qlchs.utils;
+
+import EduSys.entity.NhanVien;
+
+
+
+/**
+ *
+ * @author Admin
+ */
+public class Auth {
+    public static NhanVien user = null;
+    public static void lean(){
+        Auth.user = null;
+    }
+    public static boolean isLogin(){
+       return Auth.user != null;
+    }
+    public static boolean isManager(){
+        return Auth.isLogin() && user.isVaiTro();
+    }
+}

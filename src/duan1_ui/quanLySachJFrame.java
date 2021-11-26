553a9e6a3b87e494f5b5ba7ef7b334fdfb74dc61
DucToan <toanddph17196@fpt.edu.vn>
2021-11-26 21:25:38 +0700

    toanddph17196.QLSach

--- src/duan1_ui/quanLySachJFrame.java
+++ src/duan1_ui/quanLySachJFrame.java
@@ -5,11 +5,30 @@
  */
 package duan1_ui;
 
+import EduSys.entity.DauSach;
+import EduSys.entity.Sach;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import qlchs.dao.DauSachDAO;
+import qlchs.dao.SachDAO;
+import qlchs.utils.MsgBox;
+
 /**
  *
  * @author Admin
  */
 public class quanLySachJFrame extends javax.swing.JFrame {
+    DauSachDAO daoDS = new DauSachDAO();
+    List<DauSach> listDS = daoDS.selectAll();
+    SachDAO daoS = new SachDAO();
+    List<Sach> listSach = new ArrayList<>();
+ 
 
     /**
      * Creates new form quanLySachJFrame
@@ -18,6 +37,105 @@ public class quanLySachJFrame extends javax.swing.JFrame {
         initComponents();
         setLocationRelativeTo(null);
 //        new chaoJDialog(this, true).setVisible(true);
+    }
+    public void loadCbb() {
+        DefaultComboBoxModel model = (DefaultComboBoxModel) cbb.getModel();
+        cbb.removeAllItems();
+
+        for (DauSach x : listDS) {
+            cbb.addItem(x.getMaDauSach());
+        }
+    }
+
+    public Sach getForm() {
+        Sach cd = new Sach();
+        cd.setMaDauSach(cbb.getSelectedItem().toString());
+
+        cd.setMaSach(txtMaSach.getText());
+        cd.setGiaBan(Float.parseFloat(txtGia.getText()));
+        cd.setSoLuong(Integer.parseInt(txtSoLuong.getText()));
+        cd.setTenSach(txtTen.getText());
+        cd.setGhiChu(txtGhiChu.getText());
+        boolean a = true;
+        if (rdoCon.isSelected() == true) {
+            a = true;
+        } else {
+            a = false;
+        }
+        cd.setTrangThai(a);
+
+        return cd;
+    }
+
+    public void fillTable() {
+        DefaultTableModel model = (DefaultTableModel) tblSach.getModel();
+        model.setRowCount(0);
+        try {
+            String keyword = txtTenSach.getText();
+            listSach = daoS.selectByKeyword(keyword);
+            for (Sach s : listSach) {
+                Object row[] = {
+                    s.getMaSach(), s.getTenSach(), s.getMaDauSach(), s.getSoLuong(), s.getGiaBan(), s.getGhiChu(), s.isTrangThai() == true ? "Con Hang" : "Het Hang"
+                };
+                model.addRow(row);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Lỗi truy vấn");
+        }
+        if (listSach == null) {
+            JOptionPane.showMessageDialog(this, "Không có người học có tên: " + txtTenSach.getText());
+        }
+    }
+
+    public void insert() {
+        if (txtMaSach.getText().isEmpty() || txtTen.getText().isEmpty()
+                || txtGhiChu.getText().isEmpty()) {
+            MsgBox.alert(this, "Text trống!");
+            return;
+        } else if (rdoCon.isSelected() == false && rdoHet.isSelected() == false) {
+            MsgBox.alert(this, "Chưa chọn TrangThai");
+            return;
+        } else {
+            Sach a = getForm();
+            daoS.insert(a);
+            MsgBox.alert(this, "Thêm thành công");
+            fillTable();
+        }
+    }
+    public void update() throws SQLException{
+        
+        if (txtMaSach.getText().isEmpty() || txtTen.getText().isEmpty()
+                || txtGhiChu.getText().isEmpty()||txtGia.getText().isEmpty()||txtSoLuong.getText().isEmpty()) {
+            MsgBox.alert(this, "Text trống!");
+            return;
+        } else if (rdoCon.isSelected() == false && rdoHet.isSelected() == false) {
+            MsgBox.alert(this, "Chưa chọn TrangThai");
+            return;
+        } else {
+            Sach s = getForm();
+        daoS.update(s);
+        fillTable();
+        
+        }
+ 
+        
+        
+    
+    }
+    public void filist(int a){
+        tblSach.setRowSelectionInterval(a, a);
+        int i = tblSach.getSelectedRow();
+        txtMaSach.setText(tblSach.getValueAt(i, 0).toString());
+        txtTen.setText(tblSach.getValueAt(i, 1).toString());
+        txtSoLuong.setText(tblSach.getValueAt(i, 3).toString());
+        txtGia.setText(tblSach.getValueAt(i, 4).toString());
+        txtGhiChu.setText(tblSach.getValueAt(i, 5).toString());
+        if (tblSach.getValueAt(i, 6).toString().equals("Con Hang")) {
+            rdoCon.setSelected(true);
+        } else {
+            rdoHet.setSelected(true);
+        }
+        
     }
 
     /**
@@ -37,7 +155,6 @@ public class quanLySachJFrame extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         txtTen = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        txtMaDauSach = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
         txtSoLuong = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
@@ -51,6 +168,7 @@ public class quanLySachJFrame extends javax.swing.JFrame {
         jLabel10 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         txtGhiChu = new javax.swing.JTextArea();
+        cbb = new javax.swing.JComboBox<>();
         pnlDanhSach = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jLabel8 = new javax.swing.JLabel();
@@ -120,6 +238,11 @@ public class quanLySachJFrame extends javax.swing.JFrame {
         btnSua.setMaximumSize(new java.awt.Dimension(75, 23));
         btnSua.setMinimumSize(new java.awt.Dimension(75, 23));
         btnSua.setPreferredSize(new java.awt.Dimension(75, 23));
+        btnSua.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSuaActionPerformed(evt);
+            }
+        });
 
         btnLamMoi.setBackground(new java.awt.Color(39, 56, 120));
         btnLamMoi.setForeground(new java.awt.Color(240, 240, 240));
@@ -137,6 +260,13 @@ public class quanLySachJFrame extends javax.swing.JFrame {
         txtGhiChu.setRows(5);
         jScrollPane2.setViewportView(txtGhiChu);
 
+        cbb.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbb.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbbActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout pnlCapNhapLayout = new javax.swing.GroupLayout(pnlCapNhap);
         pnlCapNhap.setLayout(pnlCapNhapLayout);
         pnlCapNhapLayout.setHorizontalGroup(
@@ -146,7 +276,7 @@ public class quanLySachJFrame extends javax.swing.JFrame {
                     .addGroup(pnlCapNhapLayout.createSequentialGroup()
                         .addGap(23, 23, 23)
                         .addComponent(btnThem, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
                         .addComponent(btnSua, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(41, 41, 41)
                         .addComponent(btnLamMoi, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -163,16 +293,17 @@ public class quanLySachJFrame extends javax.swing.JFrame {
                                 .addGap(18, 18, 18)))
                         .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(pnlCapNhapLayout.createSequentialGroup()
-                                .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtGia, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
-                                    .addComponent(txtSoLuong)
-                                    .addComponent(txtMaDauSach))
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addGroup(pnlCapNhapLayout.createSequentialGroup()
                                 .addComponent(rdoCon)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(rdoHet)
-                                .addGap(25, 25, 25))))
+                                .addGap(25, 25, 25))
+                            .addGroup(pnlCapNhapLayout.createSequentialGroup()
+                                .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(txtGia, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+                                        .addComponent(txtSoLuong))
+                                    .addComponent(cbb, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE))))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, pnlCapNhapLayout.createSequentialGroup()
                         .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
@@ -201,7 +332,7 @@ public class quanLySachJFrame extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(txtMaDauSach, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cbb, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addGroup(pnlCapNhapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
@@ -235,6 +366,12 @@ public class quanLySachJFrame extends javax.swing.JFrame {
         jPanel3.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel8.setText("TÊN SÁCH:");
+
+        txtTenSach.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtTenSachKeyReleased(evt);
+            }
+        });
 
         btnTim.setBackground(new java.awt.Color(39, 56, 120));
         btnTim.setForeground(new java.awt.Color(240, 240, 240));
@@ -276,17 +413,32 @@ public class quanLySachJFrame extends javax.swing.JFrame {
                 "MÃ SÁCH", "TÊN SÁCH", "MÃ ĐẦU SÁCH", "SỐ LƯỢNG", "GIÁ BÁN", "TRẠNG THÁI"
             }
         ));
+        tblSach.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblSachMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tblSach);
 
         btnfisrt.setBackground(new java.awt.Color(39, 56, 120));
         btnfisrt.setForeground(new java.awt.Color(255, 255, 255));
         btnfisrt.setText("|<<");
         btnfisrt.setBorder(null);
+        btnfisrt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnfisrtActionPerformed(evt);
+            }
+        });
 
         btnPrev.setBackground(new java.awt.Color(39, 56, 120));
         btnPrev.setForeground(new java.awt.Color(204, 255, 255));
         btnPrev.setText("<<");
         btnPrev.setBorder(null);
+        btnPrev.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPrevActionPerformed(evt);
+            }
+        });
 
         btnLast.setBackground(new java.awt.Color(39, 56, 120));
         btnLast.setForeground(new java.awt.Color(240, 240, 240));
@@ -381,19 +533,89 @@ public class quanLySachJFrame extends javax.swing.JFrame {
 
     private void btnThemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThemActionPerformed
         // TODO add your handling code here:
+            insert();
     }//GEN-LAST:event_btnThemActionPerformed
 
     private void btnLamMoiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLamMoiActionPerformed
-        // TODO add your handling code here:
+        // TODO add your handling code here
+        txtGia.setText("");
+        txtGhiChu.setText("");
+        txtMaSach.setText("");
+        txtTen.setText("");
+        txtSoLuong.setText("");
     }//GEN-LAST:event_btnLamMoiActionPerformed
 
     private void btnLastActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLastActionPerformed
         // TODO add your handling code here:
+        filist(tblSach.getRowCount()-1);
     }//GEN-LAST:event_btnLastActionPerformed
 
     private void btnNextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNextActionPerformed
         // TODO add your handling code here:
+        int i = tblSach.getSelectedRow();
+        System.out.println(i);
+        if(i>=tblSach.getRowCount()-1){
+            MsgBox.alert(this, "Max");
+            return;
+        }else{
+            filist(i+1);
+        }
     }//GEN-LAST:event_btnNextActionPerformed
+
+    private void cbbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbbActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbbActionPerformed
+
+    private void txtTenSachKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtTenSachKeyReleased
+        // TODO add your handling code here:
+        fillTable();
+    }//GEN-LAST:event_txtTenSachKeyReleased
+
+    private void btnSuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSuaActionPerformed
+        // TODO add your handling code here:
+        try {
+            // TODO add your handling code here:
+            update();
+        } catch (SQLException ex) {
+            Logger.getLogger(quanLySachJFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnSuaActionPerformed
+
+    private void tblSachMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblSachMouseClicked
+        // TODO add your handling code here:
+        int i = tblSach.getSelectedRow();
+        
+        txtMaSach.setText(tblSach.getValueAt(i, 0).toString());
+        txtTen.setText(tblSach.getValueAt(i, 1).toString());
+        txtSoLuong.setText(tblSach.getValueAt(i, 3).toString());
+        txtGia.setText(tblSach.getValueAt(i, 4).toString());
+        txtGia.setText(tblSach.getValueAt(i, 5).toString());
+        if (tblSach.getValueAt(i, 6).toString().equals("Con Hang")) {
+            rdoCon.setSelected(true);
+        } else {
+            rdoHet.setSelected(true);
+        }
+        txtSoLuong.setEditable(true);
+        txtGia.setEditable(true);
+    }//GEN-LAST:event_tblSachMouseClicked
+
+    private void btnfisrtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnfisrtActionPerformed
+        // TODO add your handling code here:
+        filist(0);
+    }//GEN-LAST:event_btnfisrtActionPerformed
+
+    private void btnPrevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrevActionPerformed
+        // TODO add your handling code here:
+        int i = tblSach.getSelectedRow();
+        i--;
+        
+        if(i<=-1){
+            MsgBox.alert(this, "Min");
+            return;
+        }else{
+            filist(i);
+        }
+    }//GEN-LAST:event_btnPrevActionPerformed
 
     /**
      * @param args the command line arguments
@@ -441,6 +663,7 @@ public class quanLySachJFrame extends javax.swing.JFrame {
     private javax.swing.JButton btnTim;
     private javax.swing.JButton btnfisrt;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JComboBox<String> cbb;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -460,7 +683,6 @@ public class quanLySachJFrame extends javax.swing.JFrame {
     private javax.swing.JTable tblSach;
     private javax.swing.JTextArea txtGhiChu;
     private javax.swing.JTextField txtGia;
-    private javax.swing.JTextField txtMaDauSach;
     private javax.swing.JTextField txtMaSach;
     private javax.swing.JTextField txtSoLuong;
     private javax.swing.JTextField txtTen;
